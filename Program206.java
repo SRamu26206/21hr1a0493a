c264f638cf34fc7c05bae614dede3c8e81f52c6b
SRamu26206 <131353515+SRamu26206@users.noreply.github.com>
2023-06-02 17:56:49 +0530

    Add files via upload

---
+++ Program206.java
@@ -0,0 +1,13 @@
+//Java program to find  the ASCII value of a charecter
+import java.util.*;
+public class Program206{
+  public static viod main(String[] args){
+    char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+int ASCII var=(int)ch;
+System.out.println("ASCIIof"+ch+"is"+ASCII var);
+  }
+}
+
+    
